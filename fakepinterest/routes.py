3e887f2e0de6bf44fd300721bf15a90b9396b096
Lucas Ferreira <129186705+Lucasapn2@users.noreply.github.com>
2024-05-03 11:47:39 -0300

    update

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -5,7 +5,7 @@ from fakepinterest import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
 from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
 from fakepinterest.models import Usuario, Foto
-import  os
+import os
 from werkzeug.utils import secure_filename
 
 
